0b5933e10aff02c4a781fb91a29def8a2e255d02
Ambar Agrawal <ambar@wootric.com>
2017-05-16 21:04:41 +0545

    ex10

---
+++ ex10.rb
@@ -0,0 +1,27 @@
+# What Was That?
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+extra_fat_cat = '''
+#{tabby_cat}
+'''
+
+more_extra_fat_cat = """
+#{tabby_cat}
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+puts extra_fat_cat
+puts more_extra_fat_cat
