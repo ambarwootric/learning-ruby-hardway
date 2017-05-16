b5b0939655807e6a22de1cfe16a429075c5ee0c5
Ambar Agrawal <ambar@wootric.com>
2017-05-16 22:07:06 +0545

    ex13

---
+++ ex13.rb
@@ -0,0 +1,13 @@
+# Paramters, Unpacking, Variables
+
+# Command Line Arugment
+
+# ARGV is a argument variable
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
+# $stdin.gets.chomp whenver used with ARGV
+
